345c06f8c0886a78dee4e08e45bc19e77060d8d4
sowmik-sec <sowmik.sec@gmail.com>
2023-03-09 15:31:17 +0600

    user deleted section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+  const handleDelete = (user) => {
+    const agree = window.confirm(`Are you sure to delete ${user.name}`);
+    console.log(agree);
+    if (agree) {
+      //   console.log(`deleting user with id: ${user._id}`);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("User deleted successfully.");
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h2>Users: </h2>
-      {users.map((user) => (
+      <h2>Users: {displayUsers.length}</h2>
+      {displayUsers.map((user) => (
         <p key={user._id}>
-          {user.name} {user.email}
+          {user.name} {user.email}{" "}
+          <button onClick={() => handleDelete(user)}>X</button>
         </p>
       ))}
     </div>
