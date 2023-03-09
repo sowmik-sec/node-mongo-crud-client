ff051170950c1f37476eb5d24163bb029cbbaefb
sowmik-sec <sowmik.sec@gmail.com>
2023-03-09 12:37:17 +0600

    data showing from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+  const users = useLoaderData();
   return (
     <div>
       <h2>Users: </h2>
+      {users.map((user) => (
+        <p key={user._id}>
+          {user.name} {user.email}
+        </p>
+      ))}
     </div>
   );
 };
